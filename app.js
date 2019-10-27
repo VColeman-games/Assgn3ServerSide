4f48702c2ba63f2afacffc5c52a3cac2166d4d8a
VColeman-games <coleman_valencia@columbusstate.edu>
2019-10-27 14:25:51 +0000

    Added index #2

---
+++ app.js
@@ -0,0 +1,17 @@
+var http = require('http');
+var path = require('path');
+const express = require("express");
+var bodyParser = require("body-parser");
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.static("public"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ encoded: false}));
+
+app.get('/', function(req, res){
+    res.render("index");
+
+});
