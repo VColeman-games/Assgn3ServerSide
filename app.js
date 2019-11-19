c49e73f2751f595077b36c89b8f6c3ae2c108cc0
VColeman-games <coleman_valencia@columbusstate.edu>
2019-11-19 21:06:07 +0000

    Added random #7

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ res.render('index', {ComicImg: ComicImg, ComicTitle: ComicTitle, ComicMonth: Com
 app.get('/random', function(req, res){
   var random = Math.floor((Math.random() * ComicNumber) + 1);  
   console.log(random);
-fetch('https://xkcd.com/' + random + 'info.0.json')
+fetch('https://xkcd.com/' + random + '/info.0.json')
 .then(res => res.json())
 .then(data => {
      ComicTitle = data.title;
